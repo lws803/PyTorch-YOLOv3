3fed2a29bb1a43e89e010fe635aab21203315b51
lws803 <lws803@gmail.com>
2019-04-13 19:24:54 +0800

    Added real time detector

---
+++ detector_rt.py
@@ -0,0 +1,100 @@
+from __future__ import division
+
+from models import *
+from utils.utils import *
+from utils.datasets import *
+
+import os
+import sys
+import time
+import datetime
+import argparse
+
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+import cv2
+from PIL import Image
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--config_path', type=str, default='config/yolov3.cfg', help='path to model config file')
+parser.add_argument('--weights_path', type=str, default='weights/yolov3.weights', help='path to weights file')
+parser.add_argument('--class_path', type=str, default='data/coco.names', help='path to class label file')
+parser.add_argument('--conf_thres', type=float, default=0.8, help='object confidence threshold')
+parser.add_argument('--nms_thres', type=float, default=0.4, help='iou thresshold for non-maximum suppression')
+parser.add_argument('--batch_size', type=int, default=1, help='size of the batches')
+parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
+parser.add_argument('--img_size', type=int, default=416, help='size of each image dimension')
+opt = parser.parse_args()
+print(opt)
+
+
+cuda = torch.cuda.is_available()
+
+os.makedirs('output', exist_ok=True)
+
+# Set up model
+model = Darknet(opt.config_path, img_size=opt.img_size)
+model.load_weights(opt.weights_path)
+
+if cuda:
+    model.cuda()
+
+model.eval() # Set in evaluation mode
+
+classes = load_classes(opt.class_path) # Extracts class labels from file
+
+Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+
+cap = cv2.VideoCapture(0)
+
+
+def detect_image(img):
+    # scale and pad image
+    ratio = min(opt.img_size/img.size[0], opt.img_size/img.size[1])
+    imw = round(img.size[0] * ratio)
+    imh = round(img.size[1] * ratio)
+    img_transforms = transforms.Compose([ transforms.Resize((imh, imw)),
+         transforms.Pad((max(int((imh-imw)/2),0), max(int((imw-imh)/2),0), max(int((imh-imw)/2),0), max(int((imw-imh)/2),0)),
+                        (128,128,128)),
+         transforms.ToTensor(),
+         ])
+    # convert image to Tensor
+    image_tensor = img_transforms(img).float()
+    image_tensor = image_tensor.unsqueeze_(0)
+    input_img = Variable(image_tensor.type(Tensor))
+    # run inference on the model and get detections
+    with torch.no_grad():
+        detections = model(input_img)
+        detections = non_max_suppression(detections, 80, opt.conf_thres, opt.nms_thres)
+    return detections
+
+
+while True:
+
+    ret, frame = cap.read()
+    # Get detections
+
+
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    pilimg = Image.fromarray(frame)
+    detections = detect_image(pilimg)
+
+    print (detections)
+
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    frame = cv2.resize(frame, None,fx=0.5,fy=0.5)
+    cv2.imshow('Webcam', frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
+
+
